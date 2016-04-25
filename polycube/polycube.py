60969bac727c72c1872985ea869b16d1e3d81718
enriquesleon <enriquesleon>
2016-04-24 20:47:16 -0700

    changed how the arrays work so sorting works

--- polycube/polycube.py
+++ polycube/polycube.py
@@ -15,9 +15,13 @@ z_rot_matrix = np.matrix([[0, -1, 0],
 
 
 class Piece:
-    def __init__(self, piece_list):
+    def __init__(self, piece_list,piece_id):
         self.cubes = list()
         self.oringal_cubes_set = set()
+        """The unique positions dictionary should contain unique rows with the key
+            being a coordinate system to identify a piece+ position/orientation and the value
+            being the cubes it occupies """
+        self.unique_positions = dict()
         self.piece_list = piece_list
         self.__add_piece_list_to_set(piece_list)
         if len(self.oringal_cubes_set) != len(piece_list):
@@ -30,7 +34,7 @@ class Piece:
             x = coord % 5
             y = coord % 25 // 5
             z = coord // 25
-            cube = [[x],[y],[z]]
+            cube = [x,y,z]
             #cube = np.matrix([[x], [y], [z]])
             self.cubes.append(cube)
             print(cube)
@@ -40,9 +44,7 @@ class Piece:
             self.oringal_cubes_set.add(piece)
 
     def __rotate(self, rotation_matrix):
-        for cube in self.cubes:
-            cube = rotation_matrix.dot(cube)
-
+        self.cubes = [rotation_matrix.dot(np.matrix(cube).T).T.tolist()[0] for cube in self.cubes]
     def x_rotate(self):
         self.__rotate(x_rot_matrix)
 
@@ -51,6 +53,11 @@ class Piece:
 
     def z_rotate(self):
         self.__rotate(z_rot_matrix)
+
+    def fit_to_origin(self):
+        """This should fit a piece until the origin cube lies at 0,0,0. Not all piece should
+            have to be in bounds"""
+        pass
 
     def fit_initial_config(self):
         """From an XYZ vector, shift the position until all pieces lie in bounds"""
@@ -94,7 +101,7 @@ def enter_piece_input(piece_num):
     piece_input_line = input(
         'Enter piece input. Must be unique comma seperated Values:')
     try:
-        new_piece = validate_piece_input_string(piece_input_line)
+        new_piece = validate_piece_input_string(piece_input_line,piece_num)
     except ValueError as e:
         print(str(e))
         return None
@@ -102,15 +109,18 @@ def enter_piece_input(piece_num):
         return new_piece
 
 
-def validate_piece_input_string(piece_input):
+def validate_piece_input_string(piece_input,piece_num):
     piece_split = piece_input.split(',')
-    if len(piece_split) == 1:
-        raise ValueError('Comma Serpated Values Found')
+    try:
+        int(piece_split[0])
+    except:
+        raise ValueError('No Comma Serpated Values Found')
+        
     piece_int_list = [int(x) for x in piece_split if int(x) >= 0]
     if len(piece_int_list) != len(piece_split):
         raise ValueError('No negative values allowed')
     try:
-        new_piece = Piece(piece_int_list)
+        new_piece = Piece(piece_int_list,piece_num)
     except ValueError as e:
         raise e
     else:
